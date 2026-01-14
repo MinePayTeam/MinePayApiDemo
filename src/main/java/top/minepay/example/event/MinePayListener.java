341114267e30b0a36429335d1bddfbc2cd026316
阿然 <69817107+ranminecraft@users.noreply.github.com>
2026-01-14 10:31:12 +0800

    Update MinePayListener.java

--- src/main/java/top/minepay/example/event/MinePayListener.java
+++ src/main/java/top/minepay/example/event/MinePayListener.java
@@ -5,11 +5,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import top.minepay.api.events.*;
 import top.minepay.bean.TradeInfo;
+import top.minepay.enums.TradeType;
+import top.minepay.enums.PaymentType;
 
 /**
  * 监听事件
  *
  * @author Peter1303
+ * @author Ranica
  * @date Created in 2022/3/8 21:09
  */
 public class MinePayListener implements Listener {
@@ -19,15 +22,41 @@ public class MinePayListener implements Listener {
      * @param event 事件
      */
     @EventHandler
-    public void onMinePayTradingEvent(MinePayTradingEvent event) {
-        Bukkit.getConsoleSender().sendMessage("trading");
+    public void onMinePayPreTradingEvent(MinePayPreTradingEvent event) {
+        Bukkit.getConsoleSender().sendMessage("preTrading");
+        // 获取订单信息
         TradeInfo info = event.getTradeInfo();
-        // 设置 false 那么 MinePay 订单处理将不会执行
-        event.setHandled(false);
+        // 获取订单价格（单位：分）
+        int price = info.getPrice();
+        // 允许修改订单金额（单位：分）
+        info.setPrice(100);
+        // 获取下单玩家名字
+        String playerName = info.getPlayerName();
+        // 获取订单类型（点券或礼包）
+        TradeType tradeType = info.getTradeType();
+        // 获取付款方式（微信或支付宝）
+        PaymentType paymentType = info.getType();
+        // 设置 true 将取消订单
+        event.setCancelled(false);
     }
 
     /**
-     * 延迟取消
+     * 交易处理
+     *
+     * @param event 事件
+     */
+    @EventHandler
+    public void onMinePayTradingEvent(MinePayTradingEvent event) {
+        Bukkit.getConsoleSender().sendMessage("trading");
+        // 设置 false 那么 MinePay 将不处理订单，如二维码地图生成等
+        event.setHandled(false);
+        TradeInfo info = event.getTradeInfo();
+        // 获取用于生成二维码的文本内容
+        String qrCodeContent = info.getQrcodeContent();
+    }
+
+    /**
+     * 订单开始取消
      *
      * @param event 事件
      */
@@ -37,7 +66,7 @@ public class MinePayListener implements Listener {
     }
 
     /**
-     * 取消订单
+     * 订单已取消
      *
      * @param event 事件
      */
@@ -65,4 +94,5 @@ public class MinePayListener implements Listener {
     public void onMinePayOutdatedEvent(MinePayTradeOutDatedEvent event) {
         Bukkit.getConsoleSender().sendMessage("outdated");
     }
+
 }
